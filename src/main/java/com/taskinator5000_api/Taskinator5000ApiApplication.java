d0b97cda21bdf921c60a406b70b3ed243634ede6
Gabriel <gabrielrochasouza18@gmail.com>
2026-07-09 05:19:21 -0300

    Initial commit: estrutura inicial do projeto

---
+++ src/main/java/com/taskinator5000_api/Taskinator5000ApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskinator5000_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskinator5000ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskinator5000ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskinator5000_api/Taskinator5000ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskinator5000_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taskinator5000ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
